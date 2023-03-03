fc5ce06a38fef69d704ac4f76231c288b22ecb33
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-03 09:26:38 +0000

    added repos app

---
+++ WebApp/WebApp/Catalogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Catalogo : ICatalogo
+    {
+        public List<Livro> GetLivros()
+        {
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001", "Quem MExeu na Minha Query?", 12.99m));
+            livros.Add(new Livro("002", "Fique rico com c#", 30.99m));
+            livros.Add(new Livro("003", "Java para Baixinhos", 25.99m));
+            return livros;
+        }
+    }
+}
---
+++ WebApp/WebApp/IRelatorio.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public interface IRelatorio
+    {
+        Task Imprimir(HttpContext context);
+    }
+}
