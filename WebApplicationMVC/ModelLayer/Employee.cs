6bd4ff384fda294af70271e2a040784336decf2b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-11 23:37:55 +0100

    added http get

--- WebApplicationMVC/ModelLayer/Employee.cs
+++ WebApplicationMVC/ModelLayer/Employee.cs
@@ -3,8 +3,9 @@
     public class Employee
     {
         public int ID { get; set; }
-        public string Name { get; set; }    
+        public string? Name { get; set; }    
         public long ContactNumber { get; set; } 
-        public string Address { get; set; }
+        public string? Address { get; set; }
+        public object EmployeeID { get; set; }
     }
 }
