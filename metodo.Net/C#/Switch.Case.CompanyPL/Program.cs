d75ea1743ae663afd9798f8a24c0946906c2533e
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-08-04 10:28:49 +0100

    add

---
+++ metodo.Net/C#/Switch.Case.CompanyPL/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type 1 - If you want to contract a new plan.");
+        Console.WriteLine("Type 2 - If you want to complain about service.");
+        Console.WriteLine("Type 3 - If you want a new invoice.");
+        Console.WriteLine("Type 4 - If you want to Exit.");
+
+        var option = Console.ReadLine();
+
+        switch (option)
+        {
+            case "1":
+                Console.WriteLine("New plan information.");
+                break;
+            case "2":
+                Console.WriteLine("Tell me about your complain.");
+                break;
+            case "3":
+                Console.WriteLine("Give me your email to send you a new invoice.");
+                break;
+            case "4":
+                Console.WriteLine("See you next time.");
+                break;
+            default:
+                Console.WriteLine("Use a valid option.");
+                break;
+        }
+    }
+}
