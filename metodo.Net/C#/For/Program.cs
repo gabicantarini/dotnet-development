ef99c3393d912da363fe6f50523b829e50655c4c
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-08-05 10:55:25 +0100

    add

---
+++ metodo.Net/C#/For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ForStructure();
+    }
+
+    static void ForStructure()
+    {
+        Console.WriteLine("Escreva uma sequência de números separados por espaço: ");
+        var sentence = Console.ReadLine();
+
+        var sentenceSum = sentence.Split(' '); //remove space
+
+        Console.WriteLine("Estes são os números digitados: ");
+
+        for (int i = 0; i < sentenceSum.Length; i++)
+        {
+            Console.Write(sentenceSum[i]); //return numbers without space
+        }
+        Console.ReadKey();
+    }
+
+}
