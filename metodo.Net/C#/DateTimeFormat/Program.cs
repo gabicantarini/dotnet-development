e54ce5e54ef5411b79cac9dc079544bf99ced8aa
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-08-22 00:09:42 +0100

    add

--- metodo.Net/C#/DateTimeFormat/Program.cs
+++ metodo.Net/C#/DateTimeFormat/Program.cs
@@ -10,35 +10,7 @@ namespace DateTimeFormat
     {
         static void Main(string[] args)
         {
-            var now = DateTime.Now;
-            var Today = DateTime.Today;
-
-            var threeDaysAg = Today.AddDays(-3);
-            var sixMonthsLater = Today.AddMonths(6);
-            var twoYearsLater = Today.AddYears(2);
-
-            var shortDate = Today.ToShortDateString();
-            var longDate = Today.ToLongDateString();
-
-            var shortTime = Today.ToShortTimeString();
-            var longTime = Today.ToLongTimeString();
-
-            var date = now.Date;
-            var day = now.Day;
-            var month = now.Month;
-            var year = now.Year;
-            var hour = now.Hour;
-            var minute = now.Minute;
-            var second = now.Second;
-
-            var dayOfWeek = now.DayOfWeek;
-
-            if (dayOfWeek == DayOfWeek.Sunday || dayOfWeek == DayOfWeek.Saturday)
-            {
-                Console.WriteLine("É fim de semana!");
-            }
-
-            var dayOfYear = now.DayOfYear;
+            var format = new string[] { "d", "D", "f", "F", "g", "G", "m", "o", "r", "s", "t", "T", "u", "U", "Y" };
         }
     }
 }
