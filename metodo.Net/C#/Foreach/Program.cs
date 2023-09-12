3cfe950d2a0375b4690603636ed0dbaba409f6c6
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-12 11:26:05 +0100

    add

---
+++ metodo.Net/C#/Foreach/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        ForeachStructure();
+    }
+
+    static void ForeachStructure()
+    {
+        Console.WriteLine("Escreva uma sequência de números separados por espaço: ");
+        var numbers = Console.ReadLine();
+
+        Console.WriteLine("Estes são os números digitados usando foreach: ");
+
+        foreach(var number in numbers)
+        {
+            Console.Write(number);
+        }
+    }
+}
