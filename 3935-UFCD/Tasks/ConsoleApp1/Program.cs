f34c0dec8555173e359f75bc5b2cb8256e874bb5
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-29 17:14:08 +0100

    UFCD 3935

---
+++ 3935-UFCD/Tasks/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double length, heigh, area;
+
+        Console.WriteLine("Informe as Área do Retângulo.");
+        Console.Write("Digite o comprimento: ");
+        length = double.Parse(Console.ReadLine());
+        Console.Write("\nDigite a altura: ");
+        heigh = double.Parse(Console.ReadLine());
+
+        area = length * heigh;
+        Console.WriteLine("A área do retângulo é: " + area);
+        
+    }
+}
+
+
+ 
