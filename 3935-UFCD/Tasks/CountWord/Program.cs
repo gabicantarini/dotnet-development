24aafca6623af959427457669be9f8c3cc07d633
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-30 09:45:24 +0100

    update

---
+++ 3935-UFCD/Tasks/CountWord/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task5.CountWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountWord();
+        }
+
+        static void CountWord() {
+
+            Console.Write("Escreva uma frase: ");
+            string sentence = Console.ReadLine();
+            
+            string[] words = sentence.Split(' ');
+            int wordsSum = words.Length;
+
+            if(wordsSum > 1)
+            {
+                Console.WriteLine($"A frase digitada tem {wordsSum} palavras.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"A frase digitada tem {wordsSum} palavra.");
+                Console.ReadLine();
+
+            }                   
+
+           
+        }
+    }
+}
---
+++ 3935-UFCD/Tasks/Task4.Pythagoras/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Program
+{
+    //Pythagoras theory: hypotenuse = catetoA2 * catetoB2
+    static void Main(string[] args)
+    {
+        Pythagoras();
+
+    }
+    
+    static void Pythagoras()
+    {
+        double catetoA, catetoB, hypotenuse;
+
+        Console.Write("Digite o valor do cateto A: ");
+        catetoA = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor do cateto B: ");
+        catetoB = double.Parse(Console.ReadLine());
+        hypotenuse = Math.Sqrt(catetoA * catetoA + catetoB * catetoB);
+
+        Console.WriteLine("O valor da Hipotenusa é: {0}", hypotenuse);
+        Console.ReadLine();
+
+    }
+    
+}
