540c6d1853fdd0844d7321f03d53b65a39a04812
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-10-06 16:54:34 +0100

    added student grade

--- metodo.Net/C#/C#/Program.cs
+++ metodo.Net/C#/C#/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Channels;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Type student grade: ");
+        var gradesValue = Console.ReadLine();
+
+        var grade = int.Parse(gradesValue);
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("\n Aproved!");
+        } else if (grade >= 40)
+        {
+            Console.WriteLine("\n Second Chance.");
+        }
+        else
+        {
+            Console.WriteLine("\n Reproved!");
+        }
+    }
+}
