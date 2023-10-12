2de9c79ba787a78929ef81261ca1b66f4e8da9a6
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-10-12 11:20:46 +0100

    addname n last name

---
+++ 3935-UFCD/Tasks/Task6.NameLastName/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6.NameLastName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fullName;
+
+            Console.Write("Digite o seu nome completo: ");
+            fullName = Console.ReadLine();
+
+            var names = fullName.Split(' ');
+            string firstName = names[0];//[0] returns string first position
+            string lastName = fullName.Substring(fullName.LastIndexOf(" "));
+            //Substring: retuns the string from the indexOf position
+            //LastIndexOf: returns last index position
+
+            Console.WriteLine("Primeiro nome: " + firstName);
+            Console.Write("Último nome: " + lastName);
+
+
+            Console.ReadLine();
+        }
+    }
+}
