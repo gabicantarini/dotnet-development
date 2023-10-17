446d480072566d65ba31537dc89a87683a9f024b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-10-17 17:57:47 +0100

    add user controller

--- metodo.Net/DevFreela/DevFreela/DevFreela.API/Controllers/ProjectsController.cs
+++ metodo.Net/DevFreela/DevFreela/DevFreela.API/Controllers/ProjectsController.cs
@@ -1,6 +1,15 @@
-﻿namespace DevFreela.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
 {
-    public class ProjectsController
+    [Route("api/projects")]
+    public class ProjectsController: ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
     }
 }
+
---
+++ metodo.Net/DevFreela/DevFreela/DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+    }
+}
