4b72a45870b83ae9e2adcf42cfc2e5bf06dcee4e
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-10-19 19:54:08 +0100

    update api

--- metodo.Net/DevFreela/DevFreela/DevFreela.API/Program.cs
+++ metodo.Net/DevFreela/DevFreela/DevFreela.API/Program.cs
@@ -1,11 +1,23 @@
+using DevFreela.API.Models;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure opening time appsettings.json
+builder.Services.Configure<OpeningTimeOption>(builder.Configuration.GetSection("OpeningTime"));
+
+//confirmar
+builder.Services.AddScoped<ExampleClass>(e => new ExampleClass { Name = "Initial Stage" });
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,7 +25,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevFreela.API v1"));
 }
 
 app.UseHttpsRedirection();
