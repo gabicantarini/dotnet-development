d1599bab84189d8cab3e9ef5c921b90339337693
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-08 10:41:59 +0000

    add example

---
+++ 3935-UFCD/Tasks/ContarNumero/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContarNumero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Conta = 0;
+            string Nome;
+
+            Console.Write("Digite um nome ou ZZZ para terminar: ");
+            Nome = Console.ReadLine().ToUpper();
+
+            while (Nome != "ZZZ")
+            {
+                Conta = ContaIniciais(Nome, Conta);
+                Console.Write("Digite um nome ou ZZZ para terminar: ");
+                Nome = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine($"Há {Conta} nome(s) começado(s) por A, B ou C");
+            Console.ReadLine();
+        }
+
+        static int ContaIniciais(string nome, int contador)
+        {
+            if (nome.StartsWith("A") || nome.StartsWith("B") || nome.StartsWith("C"))
+            {
+                contador++;
+            }
+            return contador;
+        }
+    }
+}
