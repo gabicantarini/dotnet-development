b4a7484d9b88092c4571e390ff7f129ca1499e2d
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-09 10:40:12 +0000

    addad example

---
+++ 3935-UFCD/Tasks/Alfanumerico/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Alfanumerico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string X1, Y1; 
+
+            Console.WriteLine("Valores originais:");
+            Console.Write($"Digite o valor da primeira variável (X): ");
+            X1 = Console.ReadLine();
+            Console.Write($"Digite o valor da segunda variável (Y): ");
+            Y1 = Console.ReadLine();
+
+            TrocarValores(ref X1, ref Y1);
+
+            
+        }
+
+        static void TrocarValores(ref string X1, ref string Y1)
+        {
+            string temp = X1;
+            X1 = Y1;
+            Y1 = temp;
+
+            Console.WriteLine("\nValores trocados:");
+            Console.WriteLine($"X = {X1}");
+            Console.WriteLine($"Y = {Y1}");
+
+            Console.ReadLine();
+        }
+    }
+}
