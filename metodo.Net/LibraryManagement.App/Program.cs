e0aef662fea63999578bcc5d194a380a88af5ed6
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-10 11:25:32 +0000

    updated

---
+++ metodo.Net/LibraryManagement.App/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+Console.WriteLine("********************MENU INICIAL************************");
+Console.WriteLine("1. Cadastrar livro: ");
+Console.WriteLine("2. Consultar todos os livros: ");
+Console.WriteLine("3. Consultar um livro: ");
+Console.WriteLine("4. Remover livro.");
+Console.WriteLine("5. Sair.");
+
+
