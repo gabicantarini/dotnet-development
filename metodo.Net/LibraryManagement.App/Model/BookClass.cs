acd0364a8a7ceec1e2a7fc4949f99d9dfd5baa1b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-10 11:39:33 +0000

    added book model

---
+++ metodo.Net/LibraryManagement.App/Model/BookClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.App.Model
+{
+    internal class BookClass
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int PublishYear { get; set; }
+    }
+}
