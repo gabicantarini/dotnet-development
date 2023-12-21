50c454b878604f8cd9c3f7db1c3068b541221abe
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-21 23:15:00 +0000

    added delete method

--- metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Services/Implementations/ProjectServices.cs
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Services/Implementations/ProjectServices.cs
@@ -1,32 +1,39 @@
 ﻿using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
+using DevFreela.Core.Entities;
+using DevFreela.Infraestructure.Persistence;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DevFreela.Application.Services.Implementations
+
+namespace DevFreela.Application.Services.Implementations //Adicionado referência ao projeto do Core e Infraestrutura
 {
     public class ProjectServices : IProjectService
     {
-        //private readonly IProjectService _dbContext; //para que não seja alterado
-        //public ProjectServices(DevFreelaDbContext dbContext) 
-        //{ 
-        //}
+        private readonly DevFreelaDbContext _dbContext; //para que não seja alterado
+        public ProjectServices(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext; // Parallel acessar os dados do dbContext em memória
+        }
         public int Create(NewProjectInputModel inputModel)
         {
-            throw new NotImplementedException();
+            var project = new Project(inputModel.Title, inputModel.Description, inputModel.IdClient, inputModel.IdFreelancer, inputModel.TotalCost);
+            _dbContext.Projects.Add(project); //quando salvarmos a entidade no banco de dados, ele preenche e retorna o id
+            return project.Id;
         }
 
         public void CreateComment(CreateCommentInputModel inputModel)
         {
-            throw new NotImplementedException();
+            var comment = new ProjectComment(inputModel.Content, inputModel.IdProject, inputModel.IdUser);
+            _dbContext.Comments.Add(comment);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            //_dbContext.Projects.Remove(); //Remove da Lista
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id); // faz a consulta
+
+            project.Cancel(); //cancela o serviço
         }
 
         public void Finish(int id)
