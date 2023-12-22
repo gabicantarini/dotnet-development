566fe2965883aaf098d9b3d934995a55f06fa1fc
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-22 22:32:11 +0000

    removed

--- metodo.Net/bloodDonation/bloodDonation/Controllers/DonatorController.cs
+++ metodo.Net/bloodDonation/bloodDonation/Controllers/DonatorController.cs
@@ -16,7 +16,16 @@ namespace bloodDonation.Controllers
         [HttpPost] //create
         public IActionResult Post([FromBody] CreateDonator createDonator) //Cria
         {
+            //improve latter data validation like Address
+            if (createDonator.FullName.Length > 15)
+            {
+                return BadRequest();
+            }
 
+            //if (createDonator.Email.ContainsKey(email))
+            //{
+            //    return BadRequest("Este e-mail já está cadastrado como um doador.");
+            //}
 
             return CreatedAtAction(nameof(GetById), new { id = createDonator.Id, createDonator });
         }
