4f18c6fa1f517fa0d8b112b1cb6af710e5d6d3e4
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-27 21:35:04 +0000

    add input model

--- metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/ViewModels/UpdateProjectInputModel.cs
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/ViewModels/UpdateProjectInputModel.cs
@@ -8,5 +8,12 @@ namespace DevFreela.Application.ViewModels
 {
     public class UpdateProjectInputModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int IdClient { get; set; }
+        public int IdFreelancer { get; set; }
+        public decimal TotalCost { get; set; }
     }
+
 }
