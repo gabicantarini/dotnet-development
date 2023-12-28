391190e4374948846e5b60ed5d9853f7dbd4c98c
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-28 21:42:06 +0000

    add skill service

---
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Services/Implementations/SkillService.cs
@@ -0,0 +1,32 @@
+﻿using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+using DevFreela.Infraestructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class SkillService : ISkillService
+    {
+        private readonly DevFreelaDbContext _dbContext; //declara um campo privado na classe ProjectServices do tipo DevFreelaDbContext
+
+        public SkillService(DevFreelaDbContext dbContext) //esse contrutor inicializa o objeto DevFreelaDbContext
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<SkillViewModel> GetAll()
+        {
+            var skills = _dbContext.Skills;
+
+            var skillsViewModel = skills
+                .Select(s => new SkillViewModel(s.Id, s.Descritpion))
+                .ToList();
+
+            return skillsViewModel;
+        }
+    }
+}
