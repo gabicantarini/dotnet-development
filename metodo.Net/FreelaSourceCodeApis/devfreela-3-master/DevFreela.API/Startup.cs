f011683d8e1016ec2409c03237acd6ead046caee
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-29 18:11:49 +0000

    add startup

--- metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.API/Startup.cs
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.API/Startup.cs
@@ -1,17 +1,11 @@
 using DevFreela.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DevFreela.Infraestructure.Persistence;
 
 namespace DevFreela.API
 {
@@ -28,6 +22,8 @@ namespace DevFreela.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<OpeningTimeOption>(Configuration.GetSection("OpeningTime"));
+
+            services.AddSingleton<DevFreelaDbContext>();
 
             services.AddScoped<ExampleClass>(e => new ExampleClass { Name = "Initial Stage" });
 
