4fff257bccd220362cc5ac1f2251f43df378bfdc
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-05 10:06:26 +0000

    removed services

---
+++ devfreela/DevFreela.Infraestructure/Persistence/Migrations/20240205125926_AddLoginColumns.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevFreela.Infraestructure.Persistence.Migrations
+{
+    public partial class AddLoginColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descritpion",
+                table: "Skills",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Skills",
+                newName: "Descritpion");
+        }
+    }
+}
