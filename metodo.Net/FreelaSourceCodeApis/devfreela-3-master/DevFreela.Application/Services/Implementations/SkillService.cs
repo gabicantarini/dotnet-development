cf1da99b0f4ee1a0c753d9fa7de18175e1dec588
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-08 23:03:45 +0000

    implement dapper

--- metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Services/Implementations/SkillService.cs
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Services/Implementations/SkillService.cs
@@ -1,6 +1,7 @@
-﻿using DevFreela.Application.Services.Interfaces;
+﻿using Dapper;
 using DevFreela.Application.ViewModels;
 using DevFreela.Infraestructure.Persistence;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,12 @@ namespace DevFreela.Application.Services.Implementations
     public class SkillService : ISkillService
     {
         private readonly DevFreelaDbContext _dbContext; //declara um campo privado na classe ProjectServices do tipo DevFreelaDbContext
+        private readonly ISkillService _connectionString;
 
-        public SkillService(DevFreelaDbContext dbContext) //esse contrutor inicializa o objeto DevFreelaDbContext
+        public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration) //esse contrutor inicializa o objeto DevFreelaDbContext
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
 
         public List<SkillViewModel> GetAll()
