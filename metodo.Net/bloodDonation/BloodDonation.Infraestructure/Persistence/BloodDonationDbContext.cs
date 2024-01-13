1357f055a8f852cdd1f2de200b6a536a7ead6e12
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-13 11:17:17 +0000

    updated

--- metodo.Net/bloodDonation/BloodDonation.Infraestructure/Persistence/BloodDonationDbContext.cs
+++ metodo.Net/bloodDonation/BloodDonation.Infraestructure/Persistence/BloodDonationDbContext.cs
@@ -1,37 +1,47 @@
 ﻿using BloodDonation.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using System.Xml.Linq;
 
 namespace BloodDonation.Infraestructure.Persistence
 {
-    public class BloodDonationDbContext
+    public class BloodDonationDbContext : DbContext
     {
-        public BloodDonationDbContext() //fill db in memory
+        public BloodDonationDbContext(DbContextOptions<BloodDonationDbContext> options) : base(options)  
         {
-            Donators = new List<Donator>
-            {
-                new Donator("Gabriela Cantarini", "gab@hotmail.com", new DateTime(1984, 08, 23), "F", 70, "O+", "RHFACTOR"),
-                new Donator("Le Cantarini", "leb@hotmail.com", new DateTime(1986, 07, 09), "F", 93.5, "O+", "RHFACTOR"),
-                new Donator("Ceci Cantarini", "ce@hotmail.com", new DateTime(19884, 01, 10), "F", 15.3, "O+", "RHFACTOR"),
+            //fill db in memory
+            //Donators = new List<Donator>
+            //{
+            //    new Donator("Gabriela Cantarini", "gab@hotmail.com", new DateTime(1984, 08, 23), "F", 70, "O+", "RHFACTOR"),
+            //    new Donator("Le Cantarini", "leb@hotmail.com", new DateTime(1986, 07, 09), "F", 93.5, "O+", "RHFACTOR"),
+            //    new Donator("Ceci Cantarini", "ce@hotmail.com", new DateTime(19884, 01, 10), "F", 15.3, "O+", "RHFACTOR"),
 
-            };
+            //};
 
-            Donations = new List<Donation>
-            {
-                new Donation(1, new DateTime(2023, 12, 20), 1000),
-                new Donation(1, new DateTime(2023, 12, 26), 900),
-                new Donation(1, new DateTime(2023, 12, 28), 200),
+            //Donations = new List<Donation>
+            //{
+            //    new Donation(1, new DateTime(2023, 12, 20), 1000),
+            //    new Donation(1, new DateTime(2023, 12, 26), 900),
+            //    new Donation(1, new DateTime(2023, 12, 28), 200),
 
-            };
+            //};
 
-            BloodStock = new List<BloodStock>
-            {
-                
-            };
+            //BloodStock = new List<BloodStock>
+            //{
+
+            //};
         }
 
-        public List<Donator> Donators { get; set; }
-        public List<Donation> Donations { get; set; }
-        public List<BloodStock> BloodStock { get; set; }
+        public DbSet<Donator> Donators { get; set; }
+        public DbSet<Donation> Donations { get; set; }
+        public DbSet<BloodStock> BloodStock { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
     }
 }
