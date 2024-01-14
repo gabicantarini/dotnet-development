6d17f750cb69f451e260e927796ce3b221cbdf67
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-14 11:21:46 +0000

    updated devfreela

---
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Commands/CreateProject/CreateProjectCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CreateProject
+{
+    public class CreateProjectCommand //informações para cadastrar um projeto
+    {
+    }
+}
