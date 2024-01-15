7db960046f275e5ea6d81213cbf04156f4f2e3b2
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-15 07:07:15 +0000

    implement commands

--- metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Commands/CreateProject/CreateProjectCommand.cs
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Commands/CreateProject/CreateProjectCommand.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DevFreela.Application.Commands.CreateProject
 {
-    public class CreateProjectCommand //informações para cadastrar um projeto
+    public class CreateProjectCommand : IRequest<int>// CreateProjectCommand => classe com as informações para cadastrar um projeto 
     {
     }
 }
