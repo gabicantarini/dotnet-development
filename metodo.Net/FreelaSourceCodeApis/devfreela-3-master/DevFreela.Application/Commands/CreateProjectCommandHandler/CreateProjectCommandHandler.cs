d222391122bbc7a9a624a97e82e92786eaf88323
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-16 07:07:40 +0000

    implement command handler

--- metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Commands/CreateProjectCommandHandler/CreateProjectCommandHandler.cs
+++ metodo.Net/FreelaSourceCodeApis/devfreela-3-master/DevFreela.Application/Commands/CreateProjectCommandHandler/CreateProjectCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Infraestructure.Persistence;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,17 @@ using System.Threading.Tasks;
 
 namespace DevFreela.Application.Commands.CreateProjectCommandHandler
 {
-    public class CreateProjectCommandHandler //classe trata cada informação projeto cadastrado e guarda as informações no banco de dados
+    public class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand, int>//classe trata cada informação projeto cadastrado e guarda as informações no banco de dados
     {
+
+        private readonly DevFreelaDbContext _dbContext;
+        public CreateProjectCommandHandler(DevFreelaDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public Task<int> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
