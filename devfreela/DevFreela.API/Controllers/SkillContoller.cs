491417dee36702bea5ab997c4df30ecdce1af2bc
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-26 19:53:34 +0000

    added skill controller

---
+++ devfreela/DevFreela.API/Controllers/SkillContoller.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/skills")]
+    public class SkillContoller : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public SkillContoller(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+    }
+}
