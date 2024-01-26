2d8acdd8b7822f202cd41734ef05bd509f3e09ff
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-26 20:38:39 +0000

    added finish n start project

--- devfreela/DevFreela.Application/Commands/FinishProject/FinishProjectCommandHandler.cs
+++ devfreela/DevFreela.Application/Commands/FinishProject/FinishProjectCommandHandler.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevFreela.Core.Entities;
+using DevFreela.Infraestructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
 
 namespace DevFreela.Application.Commands.FinishProject
 {
-    internal class FinishProjectCommandHandler
+    public class FinishProjectCommandHandler : IRequestHandler<FinishProjectCommand, Unit>
     {
+        private readonly DevFreelaDbContext _dbContext;
+        public FinishProjectCommandHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Unit> Handle(FinishProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _dbContext.AddAsync(request.Id);
+
+            //project.Finish();
+
+            return Unit.Value;
+        }
     }
 }
+
+
--- devfreela/DevFreela.Application/Commands/StartProject/StartProjectCommandHandler.cs
+++ devfreela/DevFreela.Application/Commands/StartProject/StartProjectCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevFreela.Infraestructure.Persistence;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace DevFreela.Application.Commands.StartProject
 {
-    public class StartProjectCommandHandler
+    public class StartProjectCommandHandler : IRequestHandler<StartProjectCommand, Unit>
     {
+        private readonly DevFreelaDbContext _dbContext;
+        public StartProjectCommandHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Unit> Handle(StartProjectCommand request, CancellationToken cancellationToken)
+        {
+
+            var project = await _dbContext.AddAsync(request.Id);
+
+            //project.Start();
+
+            await _dbContext.AddAsync(project);
+
+            return Unit.Value;
+        }
     }
 }
