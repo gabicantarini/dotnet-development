3dcd8e165eae3a4dfaa6baa625a306ca28fbc02f
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-27 12:47:48 +0000

    implementing skills query handler

---
+++ devfreela/DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQueryHandler.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using DevFreela.Application.ViewModels;
+using DevFreela.Infraestructure.Persistence;
+using MediatR;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.GetAllSkills
+{
+    public class GetAllSkillsQueryHandler : IRequestHandler<GetAllSkillsQuery, List<SkillViewModel>>
+    {
+        private readonly string _connectionString;
+
+        public GetAllSkillsQueryHandler(IConfiguration configuration) //esse contrutor inicializa o objeto DevFreelaDbContext
+        {
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
+        }
+        public async Task<List<SkillViewModel>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString)) //Connection and query throw Dapper  - Use to consult streight to DB
+            {
+                sqlConnection.Open();
+
+                var script = "SELECT Id, Description FROM Skills";
+
+                var skills = await sqlConnection.QueryAsync<SkillViewModel>(script);
+
+                return skills.ToList();
+            }
+
+            // COM EF CORE
+            //var skills = _dbContext.Skills;
+
+            //var skillsViewModel = skills
+            //    .Select(s => new SkillViewModel(s.Id, s.Description))
+            //    .ToList();
+
+            //return skillsViewModel;
+        }
+    }
+}
