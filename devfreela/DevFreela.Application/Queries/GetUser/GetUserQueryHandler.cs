eb11be5d4fe99e13846543c7fc11745160e77090
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-28 12:50:16 +0000

    implementing user command handler query

--- devfreela/DevFreela.Application/Queries/GetUser/GetUserQueryHandler.cs
+++ devfreela/DevFreela.Application/Queries/GetUser/GetUserQueryHandler.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Infraestructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Application.Queries.GetUser
 {
-    internal class GetUserQueryHandler
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserViewModel>
     {
+        private readonly DevFreelaDbContext _dbContext;
+        public GetUserQueryHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<UserViewModel> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == request.Id);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new UserViewModel(user.FullName, user.Email);
+        }
     }
 }
