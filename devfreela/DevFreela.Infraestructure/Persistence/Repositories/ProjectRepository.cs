1b811a00edaa4aefdc57167c96cbefeda2b00eed
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-29 22:11:53 +0000

    added repository

---
+++ devfreela/DevFreela.Infraestructure/Persistence/Repositories/ProjectRepository.cs
@@ -0,0 +1,25 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infraestructure.Persistence.Repositories
+{
+    public class ProjectRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+
+        public ProjectRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Project>> GetAll()
+        {
+            return await _dbContext.Projects.ToListAsync();
+        }
+    }
+}
