389c68a9969170c2c7636ebe927c87d6edd6ca50
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-01-29 22:26:19 +0000

    implementing repository

--- devfreela/DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
+++ devfreela/DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Repositories;
 using DevFreela.Infraestructure.Persistence;
+using DevFreela.Infraestructure.Persistence.Repositories;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -12,19 +14,19 @@ namespace DevFreela.Application.Queries.GetAllProjects
 {
     public class GetAllProjectsQueryHandler : IRequestHandler<GetAllProjectsQuery, List<ProjectViewModel>>
     {
-        private readonly DevFreelaDbContext _dbContext;
-        public GetAllProjectsQueryHandler(DevFreelaDbContext dbContext)
+        private readonly IProjectRepository _projectRepository;
+        public GetAllProjectsQueryHandler(IProjectRepository projectRepository)
         {
-            _dbContext = dbContext;
+            _projectRepository = projectRepository;
         }
 
         public async Task<List<ProjectViewModel>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
         {
-            var projects = _dbContext.Projects;
+            var projects = await _projectRepository.GetAll();
 
-            var projectsViewModel = await projects
+            var projectsViewModel = projects
                 .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt))
-                .ToListAsync();
+                .ToList();
 
             return projectsViewModel;
         }
---
+++ devfreela/DevFreela.Infraestructure/Persistence/Repositories/SkillRepository.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infraestructure.Persistence.Repositories
+{
+    public class SkillRepository : ISkillRepository
+    {
+    }
+}
