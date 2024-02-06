abb45c51116418c1116ce297c9d9b31a93d08a5f
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-02-06 11:05:30 +0000

    fixed DTO

--- devfreela/DevFreela.Core/DTOs/SkillDTO.cs
+++ devfreela/DevFreela.Core/DTOs/SkillDTO.cs
@@ -8,6 +8,12 @@ namespace DevFreela.Core.DTOs
 {
     public class SkillDTO
     {
+        public SkillDTO(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+
         public int Id { get; set; }
         public string Description { get; set; }
     }
