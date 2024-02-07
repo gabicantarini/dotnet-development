e9287c48741e9b9f08fff2b67f5aaf7c7db70e1f
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-02-07 22:02:30 +0000

    implementing Ux Tests

---
+++ devfreela/DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Application.Commands.CreateProjectCommandHandler;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class CreateProjectCommandHandlerTests
+    {
+
+        [Fact]
+
+        public async Task InputDataIdOk_Executed_ReturnProjectId()
+        {
+            //Arrange
+            var projectRepository = new Mock<IProjectRepository>();
+
+            var createdProjectCommand = new CreateProjectCommand
+            {
+                Title = "Title Test",
+                Description = "Description Test",
+                TotalCost = 5000,
+                IdClient = 1,
+                IdFreelancer = 2,
+            };
+
+            var createdProjectCommandHandler = new CreateProjectCommandHandler(projectRepository.Object);
+
+            //Act
+            var id = await createdProjectCommandHandler.Handle(createdProjectCommand, new CancellationToken());
+
+            //Assert
+
+            Assert.True(id >= 0);
+
+            projectRepository.Verify(pr => pr.AddAsync(It.IsAny<Project>()), Times.Once);
+        }
+    }
+}
