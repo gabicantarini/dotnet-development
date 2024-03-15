00283442f5b68908d525718c0afb9d07a2ac8ee5
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-03-15 11:44:53 +0000

    added model

--- MovieApp/MovieApp/Controllers/HelloWorldController.cs
+++ MovieApp/MovieApp/Controllers/HelloWorldController.cs
@@ -13,10 +13,12 @@ public class HelloWorldController : Controller
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome(string name, int ID = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
         //return HtmlEncoder.Default.Encode($"Hello {name}, NumTime is: {numTimes}");
 
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
--- MovieApp/MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+270df94b65e4277538f821817ba9eda343f97e9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebca6458bee3820be9e1fa45eb82bc300982f780")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
