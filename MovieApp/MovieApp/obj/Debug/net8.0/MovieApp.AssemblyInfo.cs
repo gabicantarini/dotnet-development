4e625640e48f152fc516301658e9aaa973f133ce
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-03-15 12:02:20 +0000

    added initial migration

--- MovieApp/MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebca6458bee3820be9e1fa45eb82bc300982f780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00283442f5b68908d525718c0afb9d07a2ac8ee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
