0e9cf9c409c4e6a1d8c54c24a46505516c77ea41
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2025-01-19 21:29:54 +0000

    structure services route

--- Cemob/Cemob/Cemob.API/Controllers/ServicesController.cs
+++ Cemob/Cemob/Cemob.API/Controllers/ServicesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cemob.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cemob.API.Controllers
 {
@@ -6,14 +7,67 @@ namespace Cemob.API.Controllers
     [Route("api/services")]
     public class ServicesController : ControllerBase
     {
-        public IActionResult Index()
+        //Get -> api/services
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            return View();
+            return Ok();
         }
 
-        private IActionResult View()
+        //Get -> api/services?search=crm
+        [HttpGet]
+        public IActionResult Get(string search)
         {
-            throw new NotImplementedException();
+            return Ok();
+        }
+
+        //Get -> api/services/123
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        //Post -> api/services
+        [HttpPost]
+        public IActionResult Post(CreateServicesInputModel model)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
+        }
+
+        //PUT -> api/services/123
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateServicesInputModel model) 
+        {
+            return NoContent();
+        }
+
+        //DELETE -> api/services/123
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        { 
+            return NoContent();
+        }
+
+        //PUT -> api/services/123/Start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id, UpdateServicesInputModel model)
+        {
+            return NoContent();
+        }
+
+        //PUT -> api/services/123/Complete
+        [HttpPut("{id}/complete")]
+        public IActionResult Complete(int id, UpdateServicesInputModel model)
+        {
+            return NoContent();
+        }
+
+        //POST -> api/services/123/comments
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, CreateServiceCommentInputModel model) 
+        { 
+            return Ok(); 
         }
     }
 }
