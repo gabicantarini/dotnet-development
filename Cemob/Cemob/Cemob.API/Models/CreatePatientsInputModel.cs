6e53d6e0611806c7dc267d08c5f306265a42b218
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2025-01-21 10:10:42 +0000

    added Patient Input Model

---
+++ Cemob/Cemob/Cemob.API/Models/CreatePatientsInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Cemob.API.Models
+{
+    public class CreatePatientsInputModel
+    {
+        public int IdPatient { get; set; }
+        public required string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public required string LastName { get; set; }
+        public string? PatientDescription { get; set;}
+        public DateTime BirthDate { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? CPF { get; set; }
+        public string? BloodTipe { get; set; }
+        public double? Height { get; set; }
+        public double? Weight { get; set; }
+        public string? Address { get; set; }
+    }
+}
