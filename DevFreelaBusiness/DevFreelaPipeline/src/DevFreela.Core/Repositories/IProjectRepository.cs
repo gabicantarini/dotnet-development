607f3448cecb320ef47e1f34b0471f0899fd89bf
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2025-01-24 22:43:56 +0000

    add devfreela business

---
+++ DevFreelaBusiness/DevFreelaPipeline/src/DevFreela.Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevFreela.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface IProjectRepository
+    {
+        Task<List<Project>> GetAllAsync();
+        Task<Project> GetDetailsByIdAsync(int id);
+        Task<Project> GetByIdAsync(int id);
+        Task AddAsync(Project project);
+        Task AddCommentAsync(ProjectComment projectComment);
+        Task SaveChangesAsync();
+    }
+}
---
+++ DevFreelaBusiness/DevFreelaPipeline/src/DevFreela.Infrastructure/Persistence/Repositories/ProjectRepository.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        private readonly string _connectionString;
+        public ProjectRepository(DevFreelaDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
+        }
+
+        public async Task<List<Project>> GetAllAsync()
+        {
+            return await _dbContext.Projects.ToListAsync();
+        }
+
+        public async Task<Project> GetDetailsByIdAsync(int id)
+        {
+            return await _dbContext.Projects
+                .Include(p => p.Client)
+                .Include(p => p.Freelancer)
+                .Include(p => p.Comments)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task AddAsync(Project project)
+        {
+            await _dbContext.Projects.AddAsync(project);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Project> GetByIdAsync(int id)
+        {
+            return await _dbContext.Projects.SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task AddCommentAsync(ProjectComment projectComment)
+        {
+            await _dbContext.ProjectComments.AddAsync(projectComment);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
