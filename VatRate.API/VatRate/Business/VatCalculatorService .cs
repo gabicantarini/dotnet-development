ac8394b80a1cfab115aa58b5be0f2c20a853bf1c
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2025-01-25 10:11:03 +0000

    implement calc logic

--- VatRate.API/VatRate/Business/VatCalculatorService .cs
+++ VatRate.API/VatRate/Business/VatCalculatorService .cs
@@ -13,9 +13,28 @@ namespace VatRate.Business
                 gross = request.Gross.Value;
                 net = gross / (1 + request.VatRate / 100);
                 vat = gross - net;
+
+            } 
+            else if (request.Net.HasValue)
+            {
+                net = request.Net.Value;
+                vat = net * request.VatRate / 100;
+                gross = net + vat;
+            }
+            else if(request.Vat.HasValue)
+            {
+                vat = request.Vat.Value;
+                net = vat / (request.VatRate / 100);
+                gross = net + vat;
             }
 
-            return new VatCalculationResponse { Gross = gross, Vat = vat };
+            return new VatCalculationResponse 
+            { 
+                Net = Math.Round(net, 2),
+                Gross = Math.Round(gross, 2),
+                Vat = Math.Round(vat, 2),
+                VatRate = request.VatRate
+            };
         }
     }
 }
