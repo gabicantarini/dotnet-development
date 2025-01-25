3c19b7f83f94ffc9ce2cff8ef52f205da87808ed
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2025-01-25 08:05:04 +0000

    add service vat calculator

--- VatRate.API/VatRate/Business/VatCalculatorService .cs
+++ VatRate.API/VatRate/Business/VatCalculatorService .cs
@@ -1,6 +1,22 @@
-﻿namespace VatRate.Business
+﻿using VatRate.Models;
+
+namespace VatRate.Business
 {
-    public class VatCalculatorService
+    public class VatCalculatorService : IVatCalculatorService
     {
+        public VatCalculationResponse Calculate(VatCalculationResponse request)
+        {
+            decimal net = 0, gross = 0, vat = 0;
+
+            if (request.Gross.HasValue)
+            {
+                gross = request.Gross.Value;
+                net = gross / (1 + request.VatRate / 100);
+                vat = gross - net;
+            }
+
+            return new VatCalculationResponse { Gross = gross, Vat = vat };
+        }
     }
 }
+
